38a5b16572b68a30395449ed7c430f6c133d0cf7
Tom Stockwell <2060486+stocky37@users.noreply.github.com>
2023-03-06 16:17:30 +0000

    add greeting-ui app

---
+++ greeting-ui/overlays/dev/app/files/env.js
@@ -0,0 +1,3 @@
+window.env = {
+	REACT_APP_API_URL: 'https://greeting-api-demo-dev.apps.demo-gitops-cicd.emea.hcaocp.com',
+}
---
+++ greeting-ui/overlays/prod/app/files/env.js
@@ -0,0 +1,3 @@
+window.env = {
+	REACT_APP_API_URL: 'https://greeting-api-demo-prod.apps.demo-gitops-cicd.emea.hcaocp.com',
+}
